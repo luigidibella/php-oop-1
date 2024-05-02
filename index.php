84ec2cc1365acd09bec3474ab085fc2647b35dbf
luigidibella <lykos.omega@gmail.com>
2024-05-02 16:21:06 +0200

    [add] getFullGenre function

--- index.php
+++ index.php
@@ -3,21 +3,29 @@
 class Movie{
   public $name;
   public $release;
-  public $genre;
+  public $genre_1;
+  public $genre_2;
 
-  function __construct($_name, $_release, $_genre){
+  function __construct($_name, $_release, $_genre_1, $_genre_2){
     $this->name = $_name;
     $this->release = $_release;
-    $this->genre = $_genre;
+    $this->genre_1 = $_genre_1;
+    $this->genre_2 = $_genre_2;
+  }
+
+  public function getFullGenre(){
+    return $this->genre_1 . '/' . $this->genre_2;
   }
 }
 
-$Annihilation = new Movie('Annihilation', '2018', 'Sci-fi/Horror');
+$Annihilation = new Movie('Annihilation', '2018', 'Sci-fi', 'Horror');
 
-$Matrix = new Movie('Matrix', '1999', 'Azione/Sci-fi');
+$Matrix = new Movie('Matrix', '1999', 'Azione', 'Sci-fi');
 
 var_dump($Annihilation);
 var_dump($Matrix);
+var_dump($Annihilation->getFullGenre());
+var_dump($Matrix->getFullGenre());
 
 ?>
 
@@ -31,8 +39,21 @@ var_dump($Matrix);
   <title>php-oop-1</title>
 </head>
 <body>
-  <div class="container my-5">
+  <div class="container my-5 text-center">
     <h1>Movie</h1>
+    <div class="row row-cols-3">
+      <div class="col">
+        <div class="card text-start" style="width: 18rem;">
+          <!-- <img src="..." class="card-img-top" alt="..."> -->
+          <div class="card-body">
+            <h3 class="card-title">name</h3>
+            <span>release</span>
+            <span>genre</span>
+            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+          </div>
+        </div>
+      </div>
+    </div>
     
   </div>
   
